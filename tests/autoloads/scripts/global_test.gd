49b490446ed805d25af2b001d898b2cf36954df2
AlixBarreaux <you@example.com>
2025-09-20 20:22:40 +0200

    Test: Add 2 unit tests for lives
    Lives increase by increments properly
    Lives don't exceed max lives

---
+++ tests/autoloads/scripts/global_test.gd
@@ -0,0 +1,28 @@
+class_name GlobalTest
+extends GdUnitTestSuite
+
+
+# TestSuite generated from
+const __source = "res://autoloads/scripts/global.gd"
+
+
+func before_test():
+	Global.reset()
+
+
+func test_increase_lives_increments_properly():
+	Global.set_max_lives(2)
+	Global.set_lives(1)
+	Global.increase_lives()
+	assert_that(Global.lives).is_equal(2)
+
+
+func test_increase_lives_not_exceeding_max():
+	Global.set_lives(Global.max_lives)
+	Global.increase_lives()
+	assert_int(Global.lives).is_equal(Global.max_lives)
+
+
+func test_save_game() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
